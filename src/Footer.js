9a23f7ed14f06398459c343e8e6a1302ac176ab2
Aaron <aaron1venema@gmail.com>
2023-01-26 11:50:07 -0600

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer(){
 
     return(
         <footer>
-            <p>Created By <a href="https://aaronvenema.com/" target="_blank" >Aaron Venema</a></p> 
+            <p>Created By <a href="https://aaronvenema.com/" target="_blank" rel="noreferrer" >Aaron Venema</a></p> 
         </footer>
     )
 }
