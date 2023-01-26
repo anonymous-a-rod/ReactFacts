d22e39309685896540b1c7c0e4a14473b806d525
Aaron <aaron1venema@gmail.com>
2023-01-26 11:41:37 -0600

    more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import MainContent from './MainContent';
 import Header from './Header';
+import Footer from './Footer';
 
 
 
@@ -9,7 +10,11 @@ function App(){
   return (
       <div>
           <Header />
-          <MainContent />
+          <div className='body'>
+            <MainContent />
+            <Footer/>
+          </div>
+          
       </div>
   )
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>Created By <a href="https://aaronvenema.com/" target="_blank" >Aaron Venema</a></p> 
+        </footer>
+    )
+}
+
+export default Footer;
